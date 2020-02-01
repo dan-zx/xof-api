ab652cd229be858b275652e8b64b0a14723b8e93
dan-zx <danyboy_shaggy@hotmail.com>
2020-02-01 17:52:05 -0600

    Fix test

--- xof-core/src/test/kotlin/com/github/danzx/xof/core/filter/dsl/CommentsFilterDslTest.kt
+++ xof-core/src/test/kotlin/com/github/danzx/xof/core/filter/dsl/CommentsFilterDslTest.kt
@@ -1,6 +1,7 @@
 package com.github.danzx.xof.core.filter.dsl
 
 import com.github.danzx.xof.core.filter.CommentsFilter
+import com.github.danzx.xof.core.filter.NullableValue
 import com.github.danzx.xof.core.filter.PostsFilter
 
 import io.kotlintest.shouldBe
@@ -9,11 +10,11 @@ import io.kotlintest.specs.StringSpec
 class CommentsFilterDslTest : StringSpec({
 
     "commentsWith DSL should build CommentsFilter object" {
-        val expected = CommentsFilter(userId = 1, postId = 2, parentId = 3)
+        val expected = CommentsFilter(userId = 1, postId = 2, parentId = NullableValue(3))
         val actual = commentsWith {
             userId eq expected.userId
             postId eq expected.postId
-            parentId eq expected.parentId
+            parentId eq expected.parentId?.value
         }
 
         actual shouldBe expected
