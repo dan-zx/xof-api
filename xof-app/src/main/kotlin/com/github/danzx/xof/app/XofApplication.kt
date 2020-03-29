10afdb4cc250aeb3eaf09c805c43fa7fb9239f54
dan-zx <danyboy_shaggy@hotmail.com>
2020-03-29 13:15:37 -0600

    Create local and heroku profiles
    * Add specific configuration for heroku profile

--- xof-app/src/main/kotlin/com/github/danzx/xof/app/XofApplication.kt
+++ xof-app/src/main/kotlin/com/github/danzx/xof/app/XofApplication.kt
@@ -1,11 +1,17 @@
 package com.github.danzx.xof.app
 
+import com.github.danzx.xof.app.configuration.Profiles
+
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+
+import java.util.Locale
 
 @SpringBootApplication
 class XofApplication
 
 fun main(args: Array<String>) {
+    System.setProperty("spring.profiles.default", Profiles.DEFAULT)
+    Locale.setDefault(Locale.ROOT)
     SpringApplication.run(XofApplication::class.java, *args)
 }
---
+++ xof-app/src/main/kotlin/com/github/danzx/xof/app/configuration/Profiles.kt
@@ -0,0 +1,7 @@
+package com.github.danzx.xof.app.configuration
+
+object Profiles {
+    const val LOCAL = "local"
+    const val HEROKU = "heroku"
+    const val DEFAULT = LOCAL
+}
